b4c577fc2d5477296c8078a0db2c08442362ca6e
TaitoWatase2025 <uni202506_14@telework.to>
2025-10-22 13:05:50 +0900

    PlayerControllerScript作成
    プレイヤーの移動処理導入

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(PlayerInput))]
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float MoveSpeed;//移動速度
+    [SerializeField]private float RunSpeed;// 走る速度追加式
+    private CharacterController characterController;// キャラクターコントローラーコンポーネント
+    private Transform transform;// プレイヤーのTransformコンポーネント
+    private Vector3 moveVelocity;//移動速度ベクトル
+    private InputAction move;//移動入力アクション
+    
+
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        transform = GetComponent<Transform>();
+        var inputActionAsset = GetComponent<PlayerInput>().actions;
+        move = inputActionAsset.FindAction("Move");//移動アクションを取得
+
+    }
+
+    void Update()
+    {
+        var inputVector = move.ReadValue<Vector2>();//移動入力ベクトルを取得
+        moveVelocity = new Vector3(inputVector.x, 0, inputVector.y);//移動速度ベクトルを設定
+        moveVelocity = transform.TransformDirection(moveVelocity);//ワールド座標に変換
+        characterController.Move(moveVelocity * MoveSpeed * Time.deltaTime);//キャラクターを移動
+
+
+    }
+}
