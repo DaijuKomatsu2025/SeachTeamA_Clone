64de462bb89ad167dc87638330795371ab6193c1
TaitoWatase2025 <uni202506_14@telework.to>
2025-10-22 14:23:08 +0900

    モデルを追加
    キャラクターモデルを作成追加

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,13 +6,16 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float MoveSpeed;//移動速度
-    [SerializeField]private float RunSpeed;// 走る速度追加式
+    [SerializeField]
+    private float MoveSpeed;//移動速度
+    //[SerializeField]
+    //private float RunSpeed;// 走る速度追加式
     private CharacterController characterController;// キャラクターコントローラーコンポーネント
     private Transform transform;// プレイヤーのTransformコンポーネント
     private Vector3 moveVelocity;//移動速度ベクトル
     private InputAction move;//移動入力アクション
-    
+    //private InputAction sprint; // 走る入力アクション追加式
+
 
 
     void Start()
@@ -21,6 +24,7 @@ public class PlayerController : MonoBehaviour
         transform = GetComponent<Transform>();
         var inputActionAsset = GetComponent<PlayerInput>().actions;
         move = inputActionAsset.FindAction("Move");//移動アクションを取得
+        //sprint = inputActionAsset.FindAction("Sprint"); // 走るアクションを取得追加式
 
     }
 
@@ -28,9 +32,12 @@ public class PlayerController : MonoBehaviour
     {
         var inputVector = move.ReadValue<Vector2>();//移動入力ベクトルを取得
         moveVelocity = new Vector3(inputVector.x, 0, inputVector.y);//移動速度ベクトルを設定
-        moveVelocity = transform.TransformDirection(moveVelocity);//ワールド座標に変換
         characterController.Move(moveVelocity * MoveSpeed * Time.deltaTime);//キャラクターを移動
+        //移動方向に向ける
+        transform.LookAt(transform.position + new Vector3(moveVelocity.x, 0, moveVelocity.z));
 
-
+        
+        //重力処理
+        moveVelocity.y += Physics.gravity.y * Time.deltaTime;//Y方向の速度に重力を加算
     }
 }
