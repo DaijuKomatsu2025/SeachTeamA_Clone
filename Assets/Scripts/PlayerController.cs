167db1647682bdecfe90103298627af0e2987ded
TaitoWatase2025 <uni202506_14@telework.to>
2025-10-23 10:10:36 +0900

    プレイヤー関連
    追従カメラ実装（視点位置仮）
    共通ステータススクリプト作成
    ↑
    移動速度上記に変更

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,10 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(CharacterController))]
 [RequireComponent(typeof(PlayerInput))]
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : CommonStatus
 {
-    [SerializeField]
-    private float MoveSpeed;//移動速度
+    //[SerializeField]
+    //private float MoveSpeed;//移動速度
     [SerializeField]
     private Animator animator;//アニメーターコンポーネント
     private CharacterController characterController;// キャラクターコントローラーコンポーネント
@@ -32,8 +32,13 @@ public class PlayerController : MonoBehaviour
         var inputVector = move.ReadValue<Vector2>();//移動入力ベクトルを取得
         moveVelocity = new Vector3(inputVector.x, 0, inputVector.y);//移動速度ベクトルを設定
         characterController.Move(moveVelocity * MoveSpeed * Time.deltaTime);//キャラクターを移動
+
         //移動方向に向ける
         transform.LookAt(transform.position + new Vector3(moveVelocity.x, 0, moveVelocity.z));
+
+        //重力処理
+        moveVelocity.y += Physics.gravity.y * Time.deltaTime;
+
         //アニメーション処理
         animator.SetFloat("MoveSpeed", new Vector3(moveVelocity.x, 0, moveVelocity.z).magnitude);
 
