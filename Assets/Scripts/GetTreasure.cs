0a18257bdf99d1ec176d80eec1638ce18fa78068
D-Koma2 <nei4th@gmail.com>
2025-10-27 22:24:07 +0900

    宝箱のモデル更新・カメラ2種追加
    スタート用・クリア用カメラ追加

--- Assets/Scripts/GetTreasure.cs
+++ Assets/Scripts/GetTreasure.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
+using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GetTreasure : MonoBehaviour
 {
+    [SerializeField] private CinemachineCamera _camera;
+
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("お宝ゲット！！！！！！");
+        _camera.Priority.Value = 20;
         StartCoroutine(GameClear());
     }
 
