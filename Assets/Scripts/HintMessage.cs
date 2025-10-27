a1f67fc6d17a9914e42cb4244c397f2993e26523
SatoshiKitajima2025 <uni202506_06@telework.to>
2025-10-27 13:59:23 +0900

    メッセージウィンドウ用UI、フォントとそのアセットをUPしました。
    HintMessage.CS更新(MessageWindowCanvasに紐づけ)
    MessageWindowCanvas一式追加
    TMPフォントとアセット追加

--- Assets/Scripts/HintMessage.cs
+++ Assets/Scripts/HintMessage.cs
@@ -2,21 +2,33 @@ using UnityEngine;
 
 public class HintMessage : MonoBehaviour
 {
+    [SerializeField] private Canvas messageWindowCanvas;
     [SerializeField] private string[] _hintText;
     private string _currentMsg = "未設定";
+
+    private void Start()
+    {
+        if (messageWindowCanvas != null)//初期値でメッセージウィンドウキャンバスの初期値をFalseに
+        {
+            messageWindowCanvas.gameObject.SetActive(false);
+        }
+    }
+
 
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log(_currentMsg);
+        ShowMessageWindow(_currentMsg);
     }
     private void OnTriggerExit(Collider other)
     {
         Debug.Log("ヒント表示オフ");
+        HideMessageWindow(_currentMsg);
     }
 
     public void SetCurrentMessage(int num)
     {
-        if(_hintText.Length >= num)
+        if (_hintText.Length >= num)
         {
             _currentMsg = _hintText[num];
         }
@@ -26,4 +38,20 @@ public class HintMessage : MonoBehaviour
             Debug.Log("引数がメッセージ配列の要素数をオーバーしています");
         }
     }
+
+    public void ShowMessageWindow(string _currentMsg)
+    {
+        if (messageWindowCanvas != null)
+        {
+            messageWindowCanvas.gameObject.SetActive(true);
+        }
+    }
+    public void HideMessageWindow(string _currentMsg)
+    {
+        if (messageWindowCanvas != null)
+        {
+            messageWindowCanvas.gameObject.SetActive(false);
+        }
+    }
+
 }
